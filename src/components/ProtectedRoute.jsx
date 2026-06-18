2e04c8a717aac9080dce96c2e28a9ad6a9178fa0
Caio de Souza <caiodesouza.cds@gmail.com>
2026-06-18 10:20:38 -0300

    feat: cria componente de rota protegida

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,23 @@
+import { Navigate, useLocation } from "react-router";
+import { useAuth } from "../hooks/useAuth";
+import Spinner from "./ui/Spinner";
+
+function ProtectedRoute({ children }) {
+  const { isAuthenticated, loading } = useAuth();
+  const location = useLocation();
+
+  if (loading) return <Spinner />;
+
+  if (!isAuthenticated) {
+    return (
+      <Navigate
+        to={"/auth/login"}
+        state={{ from: location.pathname }}
+        replace
+      />
+    );
+  }
+  return children;
+}
+
+export default ProtectedRoute;
